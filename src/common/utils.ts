3c5811c83b5798b7e4fb2392a60cc811bdf50575
Caitson Canpintam <caitsoncanpintam@gmail.com>
2024-07-31 14:07:08 +0000

    first commit

---
+++ src/common/utils.ts
@@ -0,0 +1,20 @@
+export function somaDeDoisNumero(num1: number, num2: number): number{
+    const tmpResult =num1 + num2;
+    return tmpResult;
+}
+
+export function subtracao(num1: number, num2: number){
+    return num1 - num2;
+    // if(num1 > num2){
+    //     return num1 - num2;
+    // }else if(num1 == num2){
+    //     return num1 - num2;
+    // }else{
+    //     return num2 -num1;
+    // }
+}
+
+export function saudacao(varhelo: string): number{
+    console.log('Hola sr. :',varhelo)
+    return 1
+}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { saudacao, somaDeDoisNumero, subtracao } from "./common/utils";
+
+let nome: 'Sunto';
+
+
+const result1 = somaDeDoisNumero(3,4);
+// const result2 = subtracao(5, 12);
+
+//console.log(result)
+// console.log(result2);
+// console.log(subtracao(5, 12))
+
+console.log(saudacao('Binham'))
